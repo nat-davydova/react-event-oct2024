afc391808d0e0a38c17a99419f6975fc57e7be9d
Natali <natti.davydova@gmail.com>
2024-10-23 20:20:56 +0300

    chore: add vercel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), vercel()],
 })
